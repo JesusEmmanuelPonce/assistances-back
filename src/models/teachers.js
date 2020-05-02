5ade01ece4c3985c05ad2e8d41138464e52ac92b
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-02 12:36:58 -0500

    teachers model

---
+++ src/models/teachers.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const teachers = new Schema({
+    names:{
+        type: String,
+        trim: true
+    },
+    last_names:{
+        type: String,
+        trim: true
+    },
+    code:{
+        type: String,
+        trim: true,
+        unique: true
+    },
+    entry:{
+        type: String,
+        trim: true
+    },
+    status:{
+        type: Boolean
+    }
+})
+
+module.exports = mongoose.model('Teachers', teachers);
